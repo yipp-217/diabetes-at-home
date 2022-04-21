494f179dc57bfed86a76d956aadd542bd96da2c7
yipp-217 <76835755+yipp-217@users.noreply.github.com>
2022-04-22 00:27:04 +1000

    Add patientController

---
+++ controllers/patientController.js
@@ -0,0 +1,42 @@
+const pdb = require('../models/patients')
+
+
+const getPatient = (req, res) => {
+    const data = pdb.find(data => data.id === req.params.userId)
+
+    if (data) {
+        res.send(data)
+    }
+    else {
+        res.send([])
+    }
+}
+
+const getPatientData = (req, res) => {
+
+}
+
+const getPatientDataWeight = (req, res) => {
+
+}
+
+const getPatientDataBloodGlucoseLevel = (req, res) => {
+
+}
+
+const getPatientDataExercise = (req, res) => {
+
+}
+
+const getDosesOfInsulinTaken = (req, res) => {
+
+}
+
+module.exports = {
+    getPatient,
+    getPatientData,
+    getPatientDataWeight,
+    getPatientDataBloodGlucoseLevel,
+    getPatientDataExercise,
+    getDosesOfInsulinTaken
+}
