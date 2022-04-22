9ff8809f0721c1b3519e4cb3c9bbc8f8b7130767
yipp-217 <76835755+yipp-217@users.noreply.github.com>
2022-04-22 13:48:18 +1000

    Create clinician schema

---
+++ models/clinician.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    // definition
+})
+
+const Clinician = mongoose.model('Clinician', schema)
+module.exports = Clinician
