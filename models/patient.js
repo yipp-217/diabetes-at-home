969a9ad21c4632fed1c606798d3c72260a61c653
yipp-217 <76835755+yipp-217@users.noreply.github.com>
2022-04-23 17:26:47 +1000

    Start on patient's schema

--- models/patient.js
+++ models/patient.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose')
 
+const User = require('./user')
+
 const schema = new mongoose.Schema({
-    // definition
+    user_properties: [User],
+    clinician: String,
+
+    data: {
+
+    }
+
 })
 
 const Patient = mongoose.model('Patient', schema)
