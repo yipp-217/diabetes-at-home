2acac8472dcea8c3cdf884eb564f504192d60a7a
yipp-217 <76835755+yipp-217@users.noreply.github.com>
2022-04-25 11:54:09 +1000

    Update patient schema

--- models/patient.js
+++ models/patient.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose')
 
-const User = require('./user')
+/* Patient User */
+const patientSchema = new mongoose.Schema({
+    email: {type: String, required: true, unique:true},
+    password: {type: String, required: true},
+    nameFamily: {type: String, required: true},
+    nameGiven: {type: String},
+    nameScreen: {type: String, required: true},
+    yearBorn: {type: Number},
+    bio: String,
 
-const schema = new mongoose.Schema({
-    user_properties: [User],
-    clinician: String,
-
-    data: {
-
-    }
-
+    clinician: {type: mongoose.Schema.Types.ObjectId, ref: 'Clinician'}
 })
 
-const Patient = mongoose.model('Patient', schema)
+
+const Patient = mongoose.model('Patient', patientSchema)
+
 module.exports = Patient
