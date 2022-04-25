668083fd4c741dd63311ea67a50be7e82cbbb04a
yipp-217 <76835755+yipp-217@users.noreply.github.com>
2022-04-25 23:05:02 +1000

    Add `createNew` and `getPatients` to patientController.js

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,42 +1,80 @@
-const pdb = require('../test-data/patients')
+const {Patient} = require('../models/patient')
 
+const createNew = async (req, res, next) => {
+    /**
+    let patient = new Patient({
+        email: req.body.email,
+        password: req.body.password,
+        nameFamily: req.body.nameFamily,
+        nameGiven: req.body.nameGiven,
+        nameScreen: req.body.nameScreen,
+        yearBorn: req.body.yearBorn,
+        bio: req.body.bio,
 
-const getPatient = (req, res) => {
-    const data = pdb.find(data => data.id === req.params.userId)
+        clinician: null,
+        supportMessage: null,
 
-    if (data) {
-        res.send(data)
+        requirements: null,
+        thresholds : null,
+        patientHealthRecord: null,
+        clinicianNotes: null
+    }) */
+
+    try {
+        let patient = new Patient(req.body)
+        await patient.save()
+        return res.redirect('/patient')
+    }
+    catch (e) {
+        return next(e)
+    }
+}
+
+const getPatients = async (req, res, next) => {
+    try {
+        const patients = await Patient.find().lean()
+        return res.render('pat-all', {data: patients})
+    }
+    catch (e) {
+        return next(e)
+    }
+}
+
+// const putPat = (req, res) => {
+//     const patient = new Patient({
+//         email: "pat@mail.com",
+//         password: "pat123",
+//         nameFamily: "Pat",
+//         nameGiven: "Alice",
+//         nameScreen: "Patty",
+//         yearBorn: 2000,
+//         bio: "people called me patty.",
+//
+//         clinician: null,
+//         supportMessage: "doing great",
+//
+//         requirements: null,
+//         thresholds : null,
+//         patientHealthRecord: null,
+//         clinicianNotes: null
+//     })
+// }
+
+const getPatient = async (req, res) => {
+    const patient = await Patient.find(data => data.email === req.params.email)
+
+    if (patient) {
+        res.send(patient)
     }
     else {
         res.send([])
     }
 }
 
-const getPatientData = (req, res) => {
-
-}
-
-const getPatientDataWeight = (req, res) => {
-
-}
-
-const getPatientDataBloodGlucoseLevel = (req, res) => {
-
-}
-
-const getPatientDataExercise = (req, res) => {
-
-}
-
-const getDosesOfInsulinTaken = (req, res) => {
-
-}
 
 module.exports = {
-    getPatient,
-    getPatientData,
-    getPatientDataWeight,
-    getPatientDataBloodGlucoseLevel,
-    getPatientDataExercise,
-    getDosesOfInsulinTaken
+    createNew,
+    getPatients,
+    // putPat,
+    getPatient
 }
