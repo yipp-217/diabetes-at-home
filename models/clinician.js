ccbeab971ff7c8a735897fba78f7690bb914976a
yipp-217 <76835755+yipp-217@users.noreply.github.com>
2022-04-26 22:15:41 +1000

    Attempt to unify users via User schema

--- models/clinician.js
+++ models/clinician.js
@@ -2,14 +2,7 @@ const mongoose = require('mongoose')
 
 /* Clinician User */
 const clinicianSchema = new mongoose.Schema({
-    email: {type: String, required: true, unique:true},
-    password: {type: String, required: true},
-    nameFamily: {type: String, required: true},
-    nameGiven: {type: String},
-    nameScreen: {type: String, required: true},
-    yearBorn: {type: Number},
-    bio: String,
-
+    user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
     patients: {type: [mongoose.Schema.Types.ObjectId], ref: 'Patient'}
 })
 
