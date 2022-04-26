a791c7c49a43732b125d0488486a8ece20f26dc3
yipp-217 <76835755+yipp-217@users.noreply.github.com>
2022-04-26 22:14:02 +1000

    Add route to test out user setup

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -7,6 +7,12 @@ homeRouter.get('/', homeController.getHome)
 
 homeRouter.get('/login', homeController.getLogin)
 
+// homeRouter.post('/login', homeController.login)
+
+homeRouter.get('/users', homeController.getUsers)
+
+homeRouter.post('/users', homeController.signup)
+
 homeRouter.get('/about-diabetes', homeController.getAboutDiabetes)
 
 homeRouter.get('/about-website', homeController.getAboutWebsite)
