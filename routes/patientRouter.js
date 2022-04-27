a31526e445a256ba24a2edacd5d39347ca629a9d
yipp-217 <76835755+yipp-217@users.noreply.github.com>
2022-04-27 20:23:24 +1000

    Add edit patient data routes

--- routes/patientRouter.js
+++ routes/patientRouter.js
@@ -3,6 +3,10 @@ const patientRouter = express.Router()
 
 const patientController = require('../controllers/patientController')
 
+patientRouter.post('/edit-data/blood-glucose-level', patientController.updateBloodGlucose)
+
+patientRouter.get('/edit-data', patientController.getPatientUserEdit)
+
 patientRouter.get('/:id', patientController.getPatientUser)
 
 // default to find Pat
