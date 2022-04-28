77bd6637ec255e13a0a136018b3b2c3b4ab474ec
yipp-217 <76835755+yipp-217@users.noreply.github.com>
2022-04-29 00:26:15 +1000

    Update patient's clinician id

--- controllers/clinicianController.js
+++ controllers/clinicianController.js
@@ -6,7 +6,7 @@ const getClinicianDashboard = async (req, res, next) => {
     try {
         // Chris's object id
         const clinician = await Clinician.findOne({email: 'chris@mail.com'}).lean()
-        const patients = await Patient.find({clinician: "626908e2af5e8168d1c69f0c"}).lean()
+        const patients = await Patient.find({clinician: "0001"}).lean()
         return res.render('clinician-dashboard.hbs', {user: clinician, patients: patients})
     }
     catch (e) {
