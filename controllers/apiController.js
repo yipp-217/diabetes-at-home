34aa528f830cf647c26fb02ad7d380a59595de76
yipp-217 <76835755+yipp-217@users.noreply.github.com>
2022-05-03 18:32:40 +1000

    Register patient's clinician

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -36,14 +36,16 @@ const userSignup = async (req, res, next) => {
 const registrations = async (req, res, next) => {
     const users = [];
     const patients = await User.find({onModel: 'Patient'}).lean()
-    for (const patient of patients) {
-        const user = {namePatient: patient.nameGiven + ' ' + patient.nameFamily}
-        const clinician = await Clinician.findOne({_id: patient.clinician}).lean()
-        if (!clinician) {
+    for (const patientUser of patients) {
+        const patient = await Patient.findOne({_id: patientUser.model}).lean()
+        const clinicianUser = await User.findOne({_id: patient.clinician}).lean()
+
+        const user = {namePatient: patientUser.nameGiven + ' ' + patientUser.nameFamily}
+        if (!clinicianUser) {
             user.nameClinician = 'null'
         }
         else {
-            user.nameClinician = clinician.nameGiven + ' ' + clinician.nameFamily
+            user.nameClinician = clinicianUser.nameGiven + ' ' + clinicianUser.nameFamily
         }
         users.push(user)
     }
@@ -52,11 +54,9 @@ const registrations = async (req, res, next) => {
 
 const registerPatient = async (req, res, next) => {
     try {
-        const clinician = await User.findOne({email: req.body.emailClinician}).lean()
-
-        console.log(await User.findOne({email: req.body.emailPatient}).lean())
-        await User.updateOne({email: req.body.emailPatient}, {$set: {clinician: clinician}})
-        console.log(await User.findOne({email: req.body.emailPatient}).lean())
+        const clinicianUser = await User.findOne({email: req.body.emailClinician}).lean()
+        const patientUser = await User.findOne({email: req.body.emailPatient}).lean()
+        await Patient.updateOne({user: patientUser}, {$set: {clinician: clinicianUser}})
         return res.redirect('/api/registration')
     }
     catch(e){
