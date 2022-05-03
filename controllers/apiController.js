e68f9aac2cbc6ff3ae834ee1b2b1738e8ae8b18b
yipp-217 <76835755+yipp-217@users.noreply.github.com>
2022-05-03 15:52:45 +1000

    Dev: signup users

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,10 +1,42 @@
+const User = require('../models/user')
 const Clinician = require('../models/clinician')
 const {Patient} = require('../models/patient')
 
 
+const getUsers = async (req, res) => {
+    const users = await User.find().lean()
+    console.log('Hi')
+    return res.render('API-user-signup.hbs', {users: users})
+}
+
+const userSignup = async (req, res, next) => {
+    try {
+        const user = new User(req.body)
+        if (Boolean(req.body.clinician)) {
+            user.onModel = 'Clinician'
+            const clinician = new Clinician
+            clinician.user = user
+            await clinician.save()
+            user.model = clinician
+        }
+        else {
+            user.onModel = 'Patient'
+            const patient = new Patient
+            patient.user = user
+            await patient.save()
+            user.model = patient
+        }
+        await user.save()
+        return res.redirect('/api/user-signup')
+    }
+    catch (e) {
+        return next(e)
+    }
+}
 
 
 
 module.exports = {
-
+    getUsers,
+    userSignup
 }
--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -4,4 +4,9 @@ const apiRouter = express.Router()
 const apiController = require('../controllers/apiController')
 
 
+apiRouter.get('/user-signup', apiController.getUsers)
+
+apiRouter.post('/user-signup', apiController.userSignup)
+
+
 module.exports = apiRouter
