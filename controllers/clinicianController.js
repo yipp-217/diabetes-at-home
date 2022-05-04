5d4799b7f653f39bbd4fdbc786f39b642bc711d6
CaitlynAllan <88321877+CaitlynAllan@users.noreply.github.com>
2022-05-04 19:56:04 +1000

    Update clinician routing

--- controllers/clinicianController.js
+++ controllers/clinicianController.js
@@ -5,9 +5,23 @@ const {Patient} = require('../models/patient')
 const getClinicianDashboard = async (req, res, next) => {
     try {
         // Chris's object id
-        const clinician = await Clinician.findOne({email: 'chris@mail.com'}).lean()
-        const patients = await Patient.find({clinician: "0001"}).lean()
-        return res.render('clinician-dashboard.hbs', {user: clinician, patients: patients})
+        //const clinician = await Clinician.findOne({email: 'chris@mail.com'}).lean()
+        //const patients = await Patient.find({clinician: "0001"}).lean()
+        //return res.render('clinician-dashboard.hbs', {user: clinician, patients: patients})
+        return res.render('clinician-dashboard.hbs', {
+            user: {
+                nameScreen: "Dr. Chris"
+            }, 
+            patients: {
+                patient1: {
+                    nameGiven: "Pat",
+                    nameFamily: "Patient",
+                    bloodGlucose: 3,
+                    bloodGlucoseLowerThreshold: 1,
+                    bloodGlucoseUpperThreshold: 5
+                }
+            }
+        })
     }
     catch (e) {
         return next(e)
@@ -16,16 +30,56 @@ const getClinicianDashboard = async (req, res, next) => {
 
 const getPatientComments = async (req, res, next) => {
     try {
-        const patients = await Patient.find({clinician: "0001"}).lean()
-        return res.render('dashboard_comment.hbs', {patients: patients})
+        //const patients = await Patient.find({clinician: "0001"}).lean()
+        //return res.render('dashboard_comment.hbs', {patients: patients})
+        return res.render('clinician_dashboard_comment.hbs')
     }
     catch (e) {
         return next(e)
     }
 }
 
+const getPatient = async (req, res, next) => {
+    try {
+        return res.render('clinician_patient.hbs')
+    }
+    catch (e) {
+        return next(e)
+    }
+}
+
+const getPatientNotes = async (req, res, next) => {
+    try {
+        return res.render('clinician_patient_notes.hbs')
+    }
+    catch (e) {
+        return next(e)
+    }
+}
+
+const getPatientDataHistory = async (req, res, next) => {
+    try {
+        return res.render('clinician_data_history.hbs')
+    }
+    catch (e) {
+        return next(e)
+    }
+}
+
+const getClinicianSettings = async (req, res, next) => {
+    try {
+        return res.render('clinician_settings.hbs')
+    }
+    catch (e) {
+        return next(e)
+    }
+}
 
 module.exports = {
     getClinicianDashboard,
-    getPatientComments
+    getPatientComments,
+    getPatient,
+    getPatientNotes,
+    getPatientDataHistory,
+    getClinicianSettings,
 }
--- routes/clinicianRouter.js
+++ routes/clinicianRouter.js
@@ -8,5 +8,13 @@ clinicianRouter.get('/dashboard', clinicianController.getClinicianDashboard)
 
 clinicianRouter.get('/patient-comments', clinicianController.getPatientComments)
 
+clinicianRouter.get('/patient/clinician-notes', clinicianController.getPatientNotes)
+
+clinicianRouter.get('/patient/data-history', clinicianController.getPatientDataHistory)
+
+clinicianRouter.get('/patient', clinicianController.getPatient)
+
+clinicianRouter.get('/settings', clinicianController.getClinicianSettings)
+
 
 module.exports = clinicianRouter
