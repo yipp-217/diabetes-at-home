94f8243afbd2d9cbf0e2b02f9a1bb189229afdae
yipp-217 <76835755+yipp-217@users.noreply.github.com>
2022-05-13 00:04:41 +1000

    Cleanup user signup

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -3,36 +3,6 @@ const {Clinician} = require('../models/clinician')
 const {Patient} = require('../models/patient')
 
 
-const getUsers = async (req, res) => {
-    const users = await User.find().lean()
-    return res.render('API-user-signup.hbs', {users: users})
-}
-
-const userSignup = async (req, res, next) => {
-    try {
-        const user = new User(req.body)
-        if (Boolean(req.body.clinician)) {
-            user.onModel = 'Clinician'
-            const clinician = new Clinician
-            clinician.user = user
-            await clinician.save()
-            user.model = clinician
-        }
-        else {
-            user.onModel = 'Patient'
-            const patient = new Patient
-            patient.user = user
-            await patient.save()
-            user.model = patient
-        }
-        await user.save()
-        return res.redirect('/api/user-signup')
-    }
-    catch (e) {
-        return next(e)
-    }
-}
-
 const registrations = async (req, res, next) => {
     const users = [];
     const patients = await User.find({onModel: 'Patient'}).lean()
@@ -67,8 +37,6 @@ const registerPatient = async (req, res, next) => {
 
 
 module.exports = {
-    getUsers,
-    userSignup,
     registrations,
     registerPatient
 }
--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -4,10 +4,6 @@ const apiRouter = express.Router()
 const apiController = require('../controllers/apiController')
 
 
-apiRouter.get('/user-signup', apiController.getUsers)
-
-apiRouter.post('/signup-user', apiController.userSignup)
-
 apiRouter.get('/registration', apiController.registrations)
 
 apiRouter.post('/register-patient', apiController.registerPatient)
