e735ca52e5d9cec18a46666f6d0feeff8494eb05
yipp-217 <76835755+yipp-217@users.noreply.github.com>
2022-05-18 00:59:05 +1000

    Store new patient on clinician

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,40 +2,12 @@ const {User} = require('../models/user')
 const {Clinician} = require('../models/clinician')
 const {Patient} = require('../models/patient')
 
+
 const getUsers = async (req, res) => {
     const users = await User.find().lean()
     return res.render('API-createUser.hbs', {users: users})
 }
 
-
-const createUser = async (req, res, next) => {
-    try {
-        const user = new User(req.body)
-        // need to secure the session key
-        user.secret = "SqS8yF:Ac;<zn9YM8:=3s\",/q$9Rn9}hX\\y7&..Q!D~h'dJu5-BGKJ7#cR``\\Z^k"
-        user.dateCreated = getDateTime()
-        console.log(req.body)
-        if (Boolean(req.body.clinician)) {
-            user.onModel = 'Clinician'
-            const clinician = new Clinician
-            clinician.user = user
-            await clinician.save()
-            user.model = clinician
-        }
-        else {
-            user.onModel = 'Patient'
-            const patient = new Patient
-            patient.user = user
-            await patient.save()
-            user.model = patient
-        }
-        await user.save()
-        return res.redirect('/user/get')
-    }
-    catch (e) {
-        return next(e)
-    }
-}
 
 const createPatientUser = async (req, res, next) => {
     try {
@@ -52,20 +24,17 @@ const createPatientUser = async (req, res, next) => {
 
             const patient = new Patient
             patient.user = user
+            patient.clinician = req.user._id
             await patient.save()
+
             user.model = patient
             await user.save()
+
+            await Clinician.findById(
+                {_id: req.user._id},
+                {$push: {patients: user}}
+            )
         }
-
-        const newPatientUser = await User.findOne({email: req.body.email})
-        await Clinician.findById(
-            {_id: req.user._id},
-            {$push: {patients: newPatientUser._id}}
-        )
-        // await Patient.findOne
-        //
-        // clinician.push()
-
         return res.redirect('/clinician/dashboard')
     }
     catch (e) {
@@ -73,19 +42,13 @@ const createPatientUser = async (req, res, next) => {
     }
 }
 
-function registerPatient(cid, pid) {
-    const clinician = await Clinician.findById(cid).lean()
-    const patient = await Patient.findBy
-}
 
 const getDateTime = () => {
-    const today = new Date().toLocaleString("en-AU", {timeZone: "Australia/Melbourne"});
-    return today;
+    return new Date().toLocaleString("en-AU", {timeZone: "Australia/Melbourne"});
 }
 
 
 module.exports = {
     getUsers,
-    createUser,
     createPatientUser
 }
