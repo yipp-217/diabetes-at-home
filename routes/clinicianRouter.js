7c2f3d2b79d488c5131ca9eee0697360bd5f38c5
CaitlynAllan <88321877+CaitlynAllan@users.noreply.github.com>
2022-05-20 00:12:07 +1000

    Data validation for register patient

--- routes/clinicianRouter.js
+++ routes/clinicianRouter.js
@@ -48,7 +48,17 @@ clinicianRouter.get('/settings', homeController.isAuthenticated, homeController.
 
 clinicianRouter.get('/register-patient', homeController.isAuthenticated, homeController.hasRole('Clinician'), clinicianController.getRegisterPatient)
 
-clinicianRouter.post('/register-patient', homeController.isAuthenticated, homeController.hasRole('Clinician'), userController.createPatientUser)
+clinicianRouter.post('/register-patient', 
+                    homeController.isAuthenticated, 
+                    homeController.hasRole('Clinician'), 
+                    body('email').isEmail().not().isEmpty().escape(),
+                    body('nameGiven').not().isEmpty().escape(),
+                    body('nameFamily').not().isEmpty().escape(),
+                    body('nameScreen').not().isEmpty().escape(),
+                    body('yearBorn').isInt().not().isEmpty().escape(),
+                    body('password').not().isEmpty().escape(),
+                    body('password2').not().isEmpty().escape(),
+                    userController.createPatientUser)
 
 clinicianRouter.get('/dark-mode', homeController.isAuthenticated, homeController.hasRole('Clinician'),clinicianController.turnOnDarkMode)
 
